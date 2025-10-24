53f90dbcf94ba86ac43ea24378a7ad32f51b1ba5
Eleftherios Kypraios <ekypraio@3-F-10.42heilbronn.de>
2025-10-25 01:33:16 +0200

    calloc & substr

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,10 +6,32 @@
 /*   By: ekypraio <ekypraio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 16:52:38 by ekypraio          #+#    #+#             */
-/*   Updated: 2025/10/23 21:10:08 by ekypraio         ###   ########.fr       */
+/*   Updated: 2025/10/25 01:03:37 by ekypraio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void			*ft_calloc(size_t count, size_t size);
+void	*calloc(size_t nmemb, size_t size)
+{
+	void	*pointer;
+
+	if (count > 3696 || size > 3696)
+		return (NULL);
+	pointer = (count * size);
+	if (pointer)
+		return (NULL);
+	ft_bzero(gtr, size, *count);
+	return (pointer);
+}
+
+// // Ordnet Speicher für ein Array von num-Objekten der Größe zu
+// und initialisiert alle Bytes im zugeordneten Speicher auf null.
+// // Wenn die Zuordnung erfolgreich ist, wird ein Zeiger auf das niedrigste
+// // (erste) Byte im zugeordneten Speicherblock zurückgegeben,
+// // das für jeden Objekttyp mit grundlegender Ausrichtung
+// entsprechend ausgerichtet ist. Wenn size null ist, ist das
+// Verhalten implementierungsdefiniert
+// // (es kann ein NULL-Zeiger zurückgegeben werden,
+// // 	oder es wird ein Zeiger ungleich NULL zurückgegeben
+// der nicht für den Zugriff auf den Speicher verwendet werden darf
