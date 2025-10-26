acd4dd708312325b205244f1ebfa87224066a6c0
Eleftherios Kypraios <ekypraio@3-F-12.42heilbronn.de>
2025-10-27 00:25:35 +0100

    ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,34 @@
 /*   By: ekypraio <ekypraio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 01:06:20 by ekypraio          #+#    #+#             */
-/*   Updated: 2025/10/26 20:02:57 by ekypraio         ###   ########.fr       */
+/*   Updated: 2025/10/27 00:21:56 by ekypraio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_substr(char const *s, unsigned int start,size_t len)
-{
+#include <stdlib.h>
 
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*res;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[start] && i < len)
+		i++;
+	res = (char *)malloc(i + 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s[start + i] && i < len)
+	{
+		res[i] = s[start + i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
 }
+
 // ft_substr() erstellt einen neuen String, der ein
 // Teilstück (Substring) von s ist.
